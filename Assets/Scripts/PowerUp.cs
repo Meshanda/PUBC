466a4a873403c7cce2606d494ac962dc4f381815
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2022-12-22 11:13:15 +0100

    repared power up heal

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -11,21 +11,41 @@ public class PowerUp : NetworkBehaviour
     [SerializeField] private bool HealthBoost;
     [SerializeField] private bool ProjectileBoost;
 
-    
+
     //TODO CLEAAAAANNNNNEEEEEE;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag is "Player")
+        if (!IsServer)
+        {
+            return;
+        }
+
+        if (other != null && other.gameObject.tag is "Player")
         {
             if (HealthBoost)
             {
+                Debug.Log("heal");
                 other.gameObject.GetComponent<Health>().Heal(50);
             }
-            else if(ProjectileBoost)
+            else if (ProjectileBoost)
             {
+                Debug.Log("damage");
+                //other.gameObject.GetComponent<Health>().Heal(-50);
+
+                //marche pas ca
                 other.gameObject.GetComponent<PlayerWeaponsManager>().GetActiveWeapon().BulletsPerShot.Value = 3;
+
             }
+
+            DestroyPowerUp();
         }
     }
+
+    void DestroyPowerUp()
+    {
+        //AudioSource.PlayClipAtPoint(GetComponent<AudioSource>().clip, transform.position);
+        NetworkObject.Despawn(true);
+    }
+
 }
 
